41aa36fd6a7c63810d65486e2828a13a412d7fc8
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-01-26 21:42:24 +0530

    Update fetch.js

--- Nodejs-Express/Day2/fetch.js
+++ Nodejs-Express/Day2/fetch.js
@@ -1,3 +1,17 @@
+fetch("http://localhost:8080/products",{
+    method: "POST",
+    body:JSON.stringify({a:1}),
+    headers:{"content-type": "application/json"},
+})
+
+.then((data) => {
+    console.log("Res",data);
+})
+.catch(console.error);
+
+
+// const http = require("http");
+// const data = http.request("url", {});
 
 
 

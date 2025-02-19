0a7a0c1ec530e274d7403cf30a272bacfc2f83c1
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-02-19 15:20:55 +0530

    Update index.js

--- Day10/Mongoose/index.js
+++ Day10/Mongoose/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDB= async ()=>{
-    const conn = await mongoose.connect("mongodb+srv://rajatind12:SHALINI1234@cluster0.kcjzx.mongodb.net/");
+    const conn = await mongoose.connect("write here mongodb path");
     console.log("mongoose")
     conn.disconnect()
 }
